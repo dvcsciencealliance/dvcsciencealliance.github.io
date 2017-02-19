2e71c5bf961e691db7518683b2b0e7b96480351b
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-02-18 18:49:12 -0800

    Add some content, styling, and animation

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['splash-page', 'annoucements', 'thirdPage', 'fourthPage', 'lastPage'],
+    sectionsColor: ['#34495e', '#3498db', '#2ecc71', '#f39c12', '#1abc9c', '#ecf0f1'],
+  });
+});
