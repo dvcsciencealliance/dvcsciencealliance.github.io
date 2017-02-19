b748106f2c542194b2d57cd0a2360a8555bb1624
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-02-18 22:31:44 -0800

    Finalize layout and colors

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['splash-page', 'annoucements', 'thirdPage', 'fourthPage', 'lastPage'],
-    sectionsColor: ['#34495e', '#3498db', '#2ecc71', '#f39c12', '#1abc9c', '#ecf0f1'],
+    anchors: ['splash-page', 'annoucements', 'about-us', 'projects', 'calendar', 'resources', 'contact-us', 'footer'],
+    sectionsColor: ['#000000', '#27ae60', '#3498db', '#34495e', '#9b59b6', '#e74c3c', '#e67e22', '#16a085'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['', 'Annoucements', 'About Us', 'Projects', 'Calendar', 'Resources', 'Contact Us', '']
   });
 });
