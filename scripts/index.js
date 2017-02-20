77f1ffd8e7f1974c9bc4131f112a103fbc1520ea
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-02-19 23:39:11 -0800

    Minor changes; add in pictures of leadership team

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['', 'annoucements', 'about-us', 'undertakings', 'calendar', 'resources', 'contact-us', 'bottom'],
-    sectionsColor: ['#000000', '#27ae60', '#3498db', '#34495e', '#9b59b6', '#e74c3c', '#e67e22', '#2c3e50'],
+    anchors: ['top', 'annoucements', 'about-us', 'undertakings', 'calendar', 'resources', 'executive-board', 'bottom'],
     navigation: true,
     navigationPosition: 'right',
-    navigationTooltips: ['', 'Annoucements', 'About Us', 'Undertakings', 'Calendar', 'Resources', 'Contact Us', ''],
-    scrollOverflow: true
+    navigationTooltips: ['', 'Annoucements', 'About Us', 'Undertakings', 'Calendar', 'Resources', 'Executive Board', ''],
+    scrollOverflow: true,
+    sectionsColor: ['#000000', '#27ae60', '#3498db', '#34495e', '#9b59b6', '#e74c3c', '#e67e22', '#2c3e50']
   });
   $('#year').html(new Date().getFullYear());
 });
