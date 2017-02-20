f73ac2b845b9b648cd2c2deba5bfc0104757848a
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-02-19 17:08:31 -0800

    Add information about each department; add favicon

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['splash-page', 'annoucements', 'about-us', 'projects', 'calendar', 'resources', 'contact-us', 'footer'],
+    anchors: ['', 'annoucements', 'about-us', 'undertakings', 'calendar', 'resources', 'contact-us', 'footer'],
     sectionsColor: ['#000000', '#27ae60', '#3498db', '#34495e', '#9b59b6', '#e74c3c', '#e67e22', '#16a085'],
     navigation: true,
     navigationPosition: 'right',
-    navigationTooltips: ['', 'Annoucements', 'About Us', 'Projects', 'Calendar', 'Resources', 'Contact Us', ''],
+    navigationTooltips: ['', 'Annoucements', 'About Us', 'Undertakings', 'Calendar', 'Resources', 'Contact Us', ''],
     scrollOverflow: true
   });
 });
