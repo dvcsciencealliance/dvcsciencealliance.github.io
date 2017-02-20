8097b7341592187cf469d50917cd62ef604b3fd4
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-02-19 19:10:10 -0800

    Add footer; fix spacing issue between different sections

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['', 'annoucements', 'about-us', 'undertakings', 'calendar', 'resources', 'contact-us', 'footer'],
-    sectionsColor: ['#000000', '#27ae60', '#3498db', '#34495e', '#9b59b6', '#e74c3c', '#e67e22', '#16a085'],
+    anchors: ['', 'annoucements', 'about-us', 'undertakings', 'calendar', 'resources', 'contact-us', 'bottom'],
+    sectionsColor: ['#000000', '#27ae60', '#3498db', '#34495e', '#9b59b6', '#e74c3c', '#e67e22', '#2c3e50'],
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips: ['', 'Annoucements', 'About Us', 'Undertakings', 'Calendar', 'Resources', 'Contact Us', ''],
     scrollOverflow: true
   });
+  $('#year').html(new Date().getFullYear());
 });
