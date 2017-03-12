e59436b50f0a6d1774c65ac81490591760199c52
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-03-11 17:01:54 -0800

    Remove tooltips

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
+  $('#year').html(new Date().getFullYear());
   $('#fullpage').fullpage({
     anchors: ['top', 'annoucements', 'about-us', 'undertakings', 'calendar', 'resources', 'executive-board', 'bottom'],
-    navigation: true,
-    navigationPosition: 'right',
-    navigationTooltips: ['', 'Annoucements', 'About Us', 'Undertakings', 'Calendar', 'Resources', 'Executive Board', ''],
     scrollOverflow: true,
     sectionsColor: ['#000000', '#27ae60', '#3498db', '#34495e', '#9b59b6', '#e74c3c', '#e67e22', '#2c3e50']
   });
-  $('#year').html(new Date().getFullYear());
 });
