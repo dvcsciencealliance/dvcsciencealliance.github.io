2e1062edb225be69c66d8dc9654c3edf9cfd3125
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-03-11 17:47:35 -0800

    Change arrow and font size when screen width is small and expand column width accordingly

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#year').html(new Date().getFullYear());
   $('#fullpage').fullpage({
-    anchors: ['top', 'annoucements', 'about-us', 'undertakings', 'calendar', 'resources', 'executive-board', 'bottom'],
+    anchors: ['top', 'announcements', 'about-us', 'undertakings', 'calendar', 'resources', 'executive-board', 'bottom'],
     scrollOverflow: true,
     sectionsColor: ['#000000', '#27ae60', '#3498db', '#34495e', '#9b59b6', '#e74c3c', '#e67e22', '#2c3e50']
   });
